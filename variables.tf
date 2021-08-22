fb33925f47aee8e63fc78031d2409a9dba1b4ecd
arielkru <63583491+arielkru@users.noreply.github.com>
2021-08-22 23:10:14 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
 variable "acl_value" {
   type        = "string"
   description = "public-read"
+  default = "public-read"
 }
+
+variable "bucket" {
+  type        = "string"
+  description = "bucket"
+  default = "custom_name"
+}
+
