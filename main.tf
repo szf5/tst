f1a7e9976c86eba99d43b3ad6fe5dffdc06db621
arielkru <63583491+arielkru@users.noreply.github.com>
2021-09-02 11:33:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+#Random for TFC 
+resource "random_id" "random" {
+  keepers = {
+    uuid = uuid()
+  }
+
+  byte_length = 11
+ 
+}
+
+output "random" {
+  value = random_id.random.hex
+}
+
+#terragoat resources
 resource "aws_s3_bucket" "data" {
   # Test
   # bucket is public
